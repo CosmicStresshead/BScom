a58e057920668d91eae70f537c800820a2a4ba46
Ben Smylie <bensmylie@gmail.com>
2024-09-24 09:52:59 +0100

    Improve colour mode switch

--- src/components/ColourModeSwitch.tsx
+++ src/components/ColourModeSwitch.tsx
@@ -1,14 +1,20 @@
-import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+import { HStack, Switch, Text, useColorMode, Icon, VStack } from '@chakra-ui/react'
+import { MdOutlineLightMode, MdOutlineDarkMode } from 'react-icons/md';
 
 const ColourModeSwitch = () => {
 
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <HStack>
-        <Switch colorScheme="green" stroke="black" isChecked={colorMode === 'dark'} onChange={toggleColorMode} ></Switch>
-        <Text>Dark Mode</Text>
-    </HStack>
+    <VStack>
+      <Text fontSize={"0.75rem"}>Colour mode:</Text>
+      <HStack>
+        <Icon as={MdOutlineDarkMode} />
+        <Switch colorScheme="yellow" isChecked={colorMode === 'light'} onChange={toggleColorMode} ></Switch>
+        <Icon as={MdOutlineLightMode} />
+      </HStack>
+      {/* <Text>{colorMode.toUpperCase()}</Text> */}
+    </VStack>
   )
 }
 
